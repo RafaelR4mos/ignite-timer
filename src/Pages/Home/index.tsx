181acffea4592849d4804efefeca5ca977419e1d
Rafael <rafaelramos0401@gmail.com>
2023-05-03 13:09:49 -0300

    äprimorando os inputs

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,38 +1,58 @@
 import { Play } from "phosphor-react";
 import {
-    CountdownContainer,
-    FormContainer,
-    HomeContainer,
-    Separator,
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
 } from "./styles";
 
 export function Home() {
-    return (
-        <HomeContainer>
-            <form>
-                <FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <input id="task" />
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
-                    <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" id="minutesAmount" />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
-                    <span>minutos.</span>
-                </FormContainer>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
-                <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
-                    <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
-                </CountdownContainer>
+          <span>minutos.</span>
+        </FormContainer>
 
-                <button type="submit">
-                    <Play size={24} />
-                    Começar
-                </button>
-            </form>
-        </HomeContainer>
-    );
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
 }
